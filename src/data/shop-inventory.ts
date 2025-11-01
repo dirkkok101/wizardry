a2b8644da5cc68812d8232f9f52f734dba9dbf2d
Dirk Kok <dkok@nxgn.co.za>
2025-11-01 18:42:09 +0200

    feat(shop): add mock shop inventory data
    - 6 starter items (weapons, armor, shield)
    - Varied pricing (20-300 gold)
    - Class restrictions on weapons
    - Ready for buy/sell testing
    
    Part of Phase 5: Shop implementation

---
+++ src/data/shop-inventory.ts
@@ -0,0 +1,84 @@
+// src/data/shop-inventory.ts
+import { Item } from '../types/Item'
+import { ItemType, ItemSlot } from '../types/ItemType'
+import { CharacterClass } from '../types/CharacterClass'
+
+/**
+ * Shop inventory - items available for purchase at Boltac's Trading Post
+ */
+export const SHOP_INVENTORY: Item[] = [
+  {
+    id: 'weapon-long-sword',
+    name: 'Long Sword',
+    type: ItemType.WEAPON,
+    slot: ItemSlot.WEAPON,
+    price: 200,
+    damage: 10,
+    classRestrictions: [CharacterClass.FIGHTER, CharacterClass.LORD, CharacterClass.SAMURAI],
+    cursed: false,
+    identified: true,
+    equipped: false,
+    description: 'A well-balanced blade for warriors'
+  },
+  {
+    id: 'armor-chain-mail',
+    name: 'Chain Mail',
+    type: ItemType.ARMOR,
+    slot: ItemSlot.ARMOR,
+    price: 300,
+    defense: 4,
+    cursed: false,
+    identified: true,
+    equipped: false,
+    description: 'Interlocking metal rings provide solid protection'
+  },
+  {
+    id: 'weapon-staff',
+    name: 'Staff',
+    type: ItemType.WEAPON,
+    slot: ItemSlot.WEAPON,
+    price: 30,
+    damage: 2,
+    classRestrictions: [CharacterClass.MAGE, CharacterClass.PRIEST, CharacterClass.BISHOP],
+    cursed: false,
+    identified: true,
+    equipped: false,
+    description: 'Simple wooden staff for spellcasters'
+  },
+  {
+    id: 'armor-leather',
+    name: 'Leather Armor',
+    type: ItemType.ARMOR,
+    slot: ItemSlot.ARMOR,
+    price: 50,
+    defense: 2,
+    cursed: false,
+    identified: true,
+    equipped: false,
+    description: 'Light armor suitable for most adventurers'
+  },
+  {
+    id: 'weapon-dagger',
+    name: 'Dagger',
+    type: ItemType.WEAPON,
+    slot: ItemSlot.WEAPON,
+    price: 20,
+    damage: 3,
+    cursed: false,
+    identified: true,
+    equipped: false,
+    description: 'Small blade, easily concealed'
+  },
+  {
+    id: 'shield-wooden',
+    name: 'Shield',
+    type: ItemType.SHIELD,
+    slot: ItemSlot.SHIELD,
+    price: 100,
+    defense: 1,
+    cursed: false,
+    identified: true,
+    equipped: false,
+    description: 'Wooden shield for blocking attacks'
+  }
+]
