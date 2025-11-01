a20d8168bcfeafb63d5e5d489572573e52ec9814
Dirk Kok <dkok@nxgn.co.za>
2025-11-01 14:38:44 +0200

    chore: configure Jest testing for Angular
    - Install @angular-builders/jest and dependencies
    - Create jest.config.js with Angular preset
    - Set up Jest test environment
    - Update package.json test scripts
    - Configure tsconfig.spec.json for Jest
    
    Tests run successfully (2 tests found - expected).

---
+++ wizardry-angular/setup-jest.ts
@@ -0,0 +1,23 @@
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv();
+
+// Global test configuration
+Object.defineProperty(window, 'CSS', { value: null });
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => ({
+    display: 'none',
+    appearance: ['-webkit-appearance']
+  })
+});
+
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => ({
+    enumerable: true,
+    configurable: true
+  })
+});
